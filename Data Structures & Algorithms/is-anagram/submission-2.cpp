26edad6b3dd06ad4716786e4fc7a76aa37ae2dd1
Arun Valakottial <31961500+arunv111285@users.noreply.github.com>
2026-04-23 16:24:34 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size())
+            return false; 
+
+        // std::sort(s.begin(), s.end());
+        // std::sort(t.begin(), t.end());
+        // return s == t;
+
+        // std::unordered_map<char, int> charVsCountS, charVsCountT;
+        // for (int i = 0 ; i < s.size(); ++i)
+        // {
+        //     charVsCountS[s[i]]++;
+        //     charVsCountT[t[i]]++;
+        // }
+        // return charVsCountS == charVsCountT;
+
+        std::array<int, 26> charState = {0};
+        for (int i = 0 ; i < s.size(); ++i)
+        {
+            ++charState[s[i] - 'a'];
+            --charState[t[i] - 'a'];
+        }
+
+        for (auto val : charState)
+        {
+            if (val != 0)
+                return false;
+        }
+
+        return true;
+    }
+};
