d47e14c8d7bd409f387efb7e497b72e2410def4c
Arun Valakottial <31961500+arunv111285@users.noreply.github.com>
2026-04-24 09:05:05 +0530

    Add: two-integer-sum - submission-11

---
+++ Data Structures & Algorithms/two-integer-sum/submission-11.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // vector<pair<int, int>> A;
+        // for (int i = 0; i < nums.size(); i++) {
+        //     A.push_back({nums[i], i});
+        // }
+
+        // sort(A.begin(), A.end());
+
+        // int i = 0, j = nums.size() - 1;
+        // while (i < j) {
+        //     int cur = A[i].first + A[j].first;
+        //     if (cur == target) {
+        //         return {min(A[i].second, A[j].second),
+        //                 max(A[i].second, A[j].second)};
+        //     } else if (cur < target) {
+        //         i++;
+        //     } else {
+        //         j--;
+        //     }
+        // }
+        // return {};
+
+        std::unordered_map<int, int> valVsIndex;
+        valVsIndex.reserve(nums.size());
+        for (int i = 0 ; i < nums.size(); ++i)
+            valVsIndex[nums[i]] = i;
+
+        for (int i = 0; i < nums.size() ; ++i)
+        {
+            const auto complement = target - nums[i];
+            if (valVsIndex.count(complement) && valVsIndex[complement] != i)
+                return {i, valVsIndex[complement]};
+        }
+        return {};
+
+
+
+        return {};
+    }
+};
