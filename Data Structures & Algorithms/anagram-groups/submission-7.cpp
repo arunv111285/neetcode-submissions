9cde3b367d816e7b20d9ece60a6f04a8f0776b3d
Arun Valakottial <31961500+arunv111285@users.noreply.github.com>
2026-05-04 09:04:51 +0530

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        // std::unordered_map<string, std::vector<string>> sortedStrs;
+        // for (int i = 0; i < strs.size(); ++i)
+        // {
+        //     auto str = strs[i];
+        //     std::sort(str.begin(), str.end());
+        //     sortedStrs[str].push_back(strs[i]);
+        // }
+
+        // std::vector<std::vector<std::string>> res;
+        // for (auto [key, strs] : sortedStrs)
+        //     res.push_back(strs);
+        // return res;
+
+        std::unordered_map<std::string, std::vector<string>> keyVsStrs;
+        for (auto & str : strs)
+        {
+            std::vector<int> key(26, 0);
+            for (int i = 0 ; i < str.size() ; ++i)
+                key[str[i] - 'a']++;
+            
+            std::string strKey;
+            for (auto val : key)
+                strKey += ',' + std::to_string(val); // Comma because  without it Count array like [12, 2, 4] 
+                                                     // will produce the same result as [1,2,2,4]
+            
+            keyVsStrs[strKey].push_back(str);
+        }
+
+        std::vector<std::vector<std::string>> res;
+        for (auto [key, strs] : keyVsStrs)
+            res.push_back(strs);
+        return res;
+    }
+};
